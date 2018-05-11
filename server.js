5e38da465731f36e14972b944ddf5aafcb119bbb
andres_getial <ivanandresgt@gmail.com>
2018-05-10 21:33:38 -0500

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 5000
 // esta parte hay que cambiar 
-let users = ['oscar', 'juan', 'marcos', 'julieta'];
+let users = ['oscar', 'juan', 'marcos', 'julieta','ivan'];
 let books = [
     {titulo: 'El señor de los anillos', autor: 'J.R.R. Tolkien'},
     {titulo: "Cancion de hielo y fuego", autor: 'George RR Martin'}
@@ -30,7 +30,7 @@ app.get('/users', (req, res) => {
     res.send(users)
 })
 
-// URL para eliminar un usuario
+// URL para crear un usuario
 // http://127.0.0.1:5000/users
 app.post('/users', (req, res) => {
     let data = req.query;
